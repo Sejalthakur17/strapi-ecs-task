0f0d2f9d96605b2b753b8a6e0b9f6871838e0c08
Sejal Thakur <sejalthakurcs@gmail.com>
2026-02-20 13:41:35 +0530

    Adding CloudWatch Dashboard

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,3 +1,7 @@
+resource "aws_cloudwatch_log_group" "strapi_logs" {
+  name              = "/ecs/sejal-strapi"
+  retention_in_days = 7
+}
 resource "aws_cloudwatch_dashboard" "ecs_dashboard" {
   dashboard_name = "sejal-ecs-dashboard"
 
