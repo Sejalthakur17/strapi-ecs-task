be50baee5f26990e17138ed328c55549779ef942
Sejal Thakur <sejalthakurcs@gmail.com>
2026-02-20 13:36:01 +0530

    Adding CloudWatch Dashboard

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,8 +1,61 @@
-resource "aws_cloudwatch_log_group" "strapi_logs" {
-  name = "/ecs/sejal-strapi"
-  retention_in_days = 7
+resource "aws_cloudwatch_dashboard" "ecs_dashboard" {
+  dashboard_name = "sejal-ecs-dashboard"
 
-  tags = {
-    Name = "strapi-log-group"
-  }
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 12
+        height = 6
+
+        properties = {
+          metrics = [
+            [ "AWS/ECS", "CPUUtilization", "ClusterName", aws_ecs_cluster.sejal_cluster.name, "ServiceName", aws_ecs_service.sejal_service.name ],
+            [ ".", "MemoryUtilization", ".", ".", ".", "." ]
+          ]
+          period = 300
+          stat   = "Average"
+          region = "us-east-1"
+          title  = "ECS CPU & Memory Utilization"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 0
+        y      = 6
+        width  = 12
+        height = 6
+
+        properties = {
+          metrics = [
+            [ "AWS/ECS", "RunningTaskCount", "ClusterName", aws_ecs_cluster.sejal_cluster.name, "ServiceName", aws_ecs_service.sejal_service.name ]
+          ]
+          period = 300
+          stat   = "Average"
+          region = "us-east-1"
+          title  = "ECS Task Count"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 12
+        y      = 0
+        width  = 12
+        height = 12
+
+        properties = {
+          metrics = [
+            [ "AWS/ECS", "NetworkRxBytes", "ClusterName", aws_ecs_cluster.sejal_cluster.name, "ServiceName", aws_ecs_service.sejal_service.name ],
+            [ ".", "NetworkTxBytes", ".", ".", ".", "." ]
+          ]
+          period = 300
+          stat   = "Sum"
+          region = "us-east-1"
+          title  = "ECS Network In/Out"
+        }
+      }
+    ]
+  })
 }
